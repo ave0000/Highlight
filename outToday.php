3d841212501fb1afc28b8750b0d8d3f188050627
ascott <aver4919@rackspace.com>
2013-09-01 13:11:41 -0500

    Add parameter for selecting team

--- outToday.php
+++ outToday.php
@@ -1,7 +1,15 @@
 <?php
+
+if(isset($_REQUEST['calTeam'])) {
+	$calTeam = $_REQUEST['calTeam'];
+	//numbers only kthx
+	$calTeam = preg_replace('/[^0-9]/', '', $calTeam);
+}else{
+	$calTeam = '237';
+}
 /* Staffing Calendar Data */
 #$outtoday = json_decode(file_get_contents('https://www.staffingcalendar.com/combined/api_markedout.php?qDate=2013-04-15&team=237'));
-$outtoday = json_decode(file_get_contents('https://www.staffingcalendar.com/combined/api_markedout.php?team=237'));
+$outtoday = json_decode(file_get_contents('https://www.staffingcalendar.com/combined/api_markedout.php?team='.$calTeam));
 $outfields = array(
     "firstName"=>"First",
     "lastName"=>"Last",
