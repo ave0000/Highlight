bdf046c2c4ab8e9252f9376cdf6e67e7b5502f13
ascott <aver4919@rackspace.com>
2013-09-11 01:03:55 -0500

    made it actually work

--- score/score.php
+++ score/score.php
@@ -6,8 +6,8 @@
  *  in oneview's slick.php
  */
 
-function score_sort($a,$b) {
-        return $a->score < $b->score;
+function scoreSort($a,$b) {
+	return $a->score < $b->score;
 }
 
 // Do scoring
@@ -16,57 +16,59 @@ function score_sort($a,$b) {
 	// status
 	// age_seconds
 	// category
-	//also need:
+	// sev
+	// is_cloud
+	//also MAY need:
 	// whatever data Modifiers call for
-
-function getScore($ticket){
-        $d = $ticket;
-
+function getScore($t,$profile='null'){
 	//maintenances to the top
-        if (preg_match('/^SCHLD \d+.*/', $d->subject))
-        	return '-';
+    if (preg_match('/^SCHLD \d+.*/', $t->subject))
+    	return '-';
 
-        $base_score = 0;
 	$feedBack = 'Feedback Received';
-
-	$subject_scores = array(
+	$categoryMatch = '/.*customer_initiated.*/i';
+	$subjectMatch = array( //MUST be desc sorted
 		'/.*ALERT:Rackwatch.*All.*Services.*/i'=>1200,
-		'/ALERT:.*SiteScope.*/i'=>800
+		'/ALERT:.*SiteScope.*/i'=> 800
 	);
 
+	$base_score = 0;
+
+    // customer feedback
+    if(preg_match($categoryMatch, $t->category))
+        $base_score = 600;
+	elseif($t->status == $feedBack)
+		$base_score = 600;
+
 	//test for some subject strings
-	foreach($subject_scores as $s=>$score){
-		if(preg_match($s,$d->subject))
-			$base_score += $score;
-	}
+	foreach($subjectMatch as $s=>$score)
+		if(preg_match($s,$t->subject)){
+			$base_score = $score;
+			break;
+		}
 
+	//create severity for cloud tickets
+    if ($t->iscloud)
+    	if (preg_match('/Monitoring Alert .*/', $t->subject))
+			$t->sev = 'emergency';
 
-        // These get a high base score!
-        if(preg_match('/.*customer_initiated.*/i', $d['category']))
-                $base_score += 600;
-	else if($d->status == $feedBack){
-		$base_score += 600;
-	}
+	//bolster score with severity
+	if($t->sev =='emergency')
+		$points = 35;
+	elseif($t->sev =='urgent')
+		$points = 9;
+	else
+		$points = 2.25;
 
-	// cloudy cloud cloud
-        $points = 2.25;
-        if ($d->iscloud) {
-        	if (preg_match('/Monitoring Alert .*/', $d->subject))
-			$points = 35;
-        }
-
-        // score primarily based on time, 
-	// at least two minutes
-        $minticks = ( $d->age_seconds / 60) * 2;
+    // score primarily based on minutes ... at least 2.
+    $minticks = ( $t->age_seconds / 60) * 2;
 	$minticks = max(2,$minticks);
-
-	//madness
-	$modifier = getScoreModifier($d);
-
-
-        $score = $base_score + ($minticks * $points);
-	$score *= $modifier;
-        $score = intval($score);
+	
+	//data collected, time to calc
+    $score = $base_score + ($minticks * $points);
+    if($profile)
+		$score *= getScoreModifier($t,$profile);
+    $score = intval($score);
 
 	return $score;
 }
@@ -74,20 +76,17 @@ function getScore($ticket){
 // Modifier
 // allows for per-queue customization
 // qs is the list of queue profiles
-// some of them have a 'modifier' array
-// modifier arrays SHOULD be pairs of index and filters
-// filters are attribute regex pairs
-// treating qs as user input
-
+// which MAY have a 'modifier' array
+// which SHOULD be "index"->filters pairs
+// filters MUST be attribute regex pairs
 function getScoreModifier($ticket,$profile){
+	//a possible strategy would be to build a cacheable function
 	$modifier = 1;
 
 	//look up profile
-	if(is_array($qs) && isset($qs[$profile]) && isset($qs[$profile]['modifier'])) {
-		$mods = $qs[$profile]['modifier'];
-	}else
+	if(!is_array($qs) || !isset($qs[$profile]) || !isset($qs[$profile]['modifier']))
 		return $modifier;
-
+	$mods = $qs[$profile]['modifier'];
 	if(!is_array($mods))
 		$mods = array($mods);
 
@@ -98,17 +97,12 @@ function getScoreModifier($ticket,$profile){
 	if(!@count($mods))
 		return $modifier;
 
-        foreach($mods as $mod) {
-                if(isset($mod['filter']) && isset($mod['index'])) {
-                        foreach($mod['filter'] as $attribute=>$regex) {
-                                if(isset($d->{$attribute}) && preg_match($regex, $d->{$attribute})) {
-                                        $modifier *= $mod['index'];
-				}
-                        }
-                }
-        }
+    foreach($mods as $mod)
+        if(isset($mod['filter']) && isset($mod['index']))
+            foreach($mod['filter'] as $attribute=>$regex)
+                if(isset($t->{$attribute}) && preg_match($regex, $t->{$attribute}))
+                    $modifier *= $mod['index'];
 
 	return $modifier;
 }
-
 ?>
