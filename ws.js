53b0afa304f62704ce1c1971dd6533fd7f6a3fd5
Avery Scott <aver4919@rackspace.com>
2013-11-05 03:50:36 +0000

    accept any up/lower case for redis commands

--- ws.js
+++ ws.js
@@ -2,13 +2,14 @@ var redis = require('redis');
 require('console-ten').init(console);
 
 var WebSocketServer = require('ws').Server
-  , wss = new WebSocketServer({port: 3000});
+  , wss = new WebSocketServer({port: 3001});
 
 wss.on('connection', function(ws) {try{
     var clientHost = ws.upgradeReq.headers['x-forwarded-for'] || ws.upgradeReq.connection.remoteAddress;
 	console.log(clientHost+' new connection');
 
-	var db = redis.createClient('/dev/shm/redis.sock');
+	var db;
+    db = redis.createClient('/dev/shm/redis.sock');
     db.on("error", function(err) {
       var msg = clientHost+"Error connecting to redis";
       console.error(msg, err);
@@ -19,15 +20,17 @@ wss.on('connection', function(ws) {try{
         try {parsed = JSON.parse(message);}
         catch(e) {ws.send("Error: I didn't understand that json");}
 
-        if(parsed) {
-        switch(parsed[0]) {
-            case "GET":
-                db.get(parsed[1],function(err,reply) {ws.send(reply);});
+        if(parsed && parsed[0] && parsed[0].toLowerCase) {
+        switch(parsed[0].toLowerCase()) {
+            case "get":
+                db.get(parsed[1],function(err,reply) {if(reply) ws.send(reply);});
                 break;
-            case "PSUBSCRIBE":
+            case "rpush":
+                db.rpush(parsed[1],parsed[2]);
+                break;
+            case "psubscribe":
                 //subscribe blocks the redis connection
-                //probably should create a new redis connect here
-                //
+                //maybe create a new redis connect here?
                 db.PSUBSCRIBE(parsed[1]);
                 db.on("pmessage",function(pattern,channel,message){
                     try{
@@ -36,21 +39,14 @@ wss.on('connection', function(ws) {try{
                         console.log('error sending pubished message to '+clientHost);
                         console.log(e);}
                 })
+                break;
 
-                break;
-            case "rpush":
-                db.rpush(parsed[1],parsed[2]);
-                break;
             case "stats":
-                console.log(clientHost+' requested stats');
-                ws.send('Dumping stats');
-                ws.send();
-                ws.send('=== Server Level ===');
-                ws.send(JSON.stringify(wss.clients));
+                stats(ws,wss,db);
                 break;
             default:
                 console.log('received: %s', message);
-        }}
+        }}else console.log("Didn't understand %s", message);
     });
     ws.on('close',function(){
         console.log(clientHost+' closed connection');
@@ -59,7 +55,19 @@ wss.on('connection', function(ws) {try{
             db.end();
         }
     });
-    //ws.send('wordl');
 }catch(e) {console.log(e);}
 });
 
+function stats(ws,wss,db) {
+    var clientHosts = []
+    console.log('client requested stats');
+    for (var i in wss.clients) {
+        var c = wss.clients[i];
+        clientHosts.push(c.upgradeReq.headers['x-forwarded-for'] || c.upgradeReq.connection.remoteAddress);
+        
+    }
+    ws.send(JSON.stringify(clientHosts));
+    //ws.send(JSON.stringify(ws,replacer,2));
+    //ws.send('=== Server Level ===');
+    //ws.send(JSON.stringify(wss.clients,replacer,2));
+}
