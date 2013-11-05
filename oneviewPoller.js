8ca13c91ea92563880878bb9edb962d879646bfd
Avery Scott <aver4919@rackspace.com>
2013-11-05 03:57:12 +0000

    node based replacement for highlightDaemon.php

---
+++ oneviewPoller.js
@@ -0,0 +1,134 @@
+var http = require("http");
+var redis = require('redis');
+require('console-ten').init(console);
+
+var db = redis.createClient('/dev/shm/redis.sock');
+db.on("error", function(err) {
+  console.error(clientHost+"Error connecting to redis", err);
+});
+
+function saveTickets(db,profile,tickets) {
+	var fields = [
+		"OnWatch",
+		"account",
+		"aname",
+		"fepochtime",
+		"age_seconds",
+		"category",
+		"iscloud",
+		"platform",
+		"sev",
+		"status",
+		"subject",
+		"team",
+		"ticket",
+		];
+	var out = [];
+	var len = tickets.length;
+	var t, store, field;
+	while(len--) {
+		t = tickets[len];
+		store = {};
+		for(var i=0;i<fields.length;i++){
+			field = fields[i];
+			store[field] = t[field];
+		}
+		db.hmset('testticket:'+t.ticket,store);
+		out.push(t.ticket);
+	}
+	return out;
+}
+
+function saveTicketList(db,profile,tickets) {
+	var name = 'testticketList:'+profile;
+	saveWithStamp(db,name,tickets);
+}
+
+function saveSummary(db,profile,data) {
+	var sum = data.summary;
+	var num = 1;
+	while(num<100 && sum['latency_'+num] === undefined)
+		num++; //grep around in the dark
+	var latstr = 'latency_'+num;
+
+	var name = 'testsummary:'+profile+':'+latstr;
+	var summary = {
+		profile: profile,
+		totalCount: sum.total_count,
+		timeStamp: Date.now(),
+		latency: sum[latstr]
+	}
+	saveWithStamp(db,name,summary);
+}
+
+function saveWithStamp(db,name,data) {
+	var now = Date.now();
+	data = JSON.stringify(data);
+	db.set(name,data);
+	db.set(name+':timestamp',now);
+	db.publish(name,data);
+}
+
+function processQueue(data) {
+	data = JSON.parse(data); // <-- need to wrap
+
+	var profile = data.summary.profile_name;
+	
+	var multi = db.multi(); //lay in a course
+	saveSummary(multi,profile,data);
+	var ticketList = saveTickets(multi,profile,data.queue);
+	saveTicketList(multi,profile,ticketList);
+	
+	//good place for a retry loop
+	multi.exec(); // make it so
+	//return console.log("Breakpoint");
+	process.nextTick(popNext);
+}
+
+function fetchSummary(profile,latency) {
+	var slick = 'http://oneview.rackspace.com/slick.php';
+	var url = slick + '?fmt=json&latency='+latency+'&profile='+encodeURIComponent(profile);
+
+	http.get(url,function(res){
+		var data = '';
+		res.on('data',function(chunk){data+=chunk;}); //may barf on unicode at chunk boundry?
+		res.on('end',function(){processQueue(data);});
+	}).on('error',function(e){
+		console.log("Error: %s",e.message);
+	}).setTimeout(30000);
+}
+
+//If timestamp was more than limit ago, then it's stale
+function isFresh(timeStamp,limit) {
+	if(!timeStamp) return false;
+	if(typeof(limit)==='undefined') limit = 9000;
+	var now = Date.now();
+	var diff = now - timeStamp;
+	return !(diff > limit); //that's confusing, why'd i write it that way?
+}
+
+function newSummary(d) {
+	if(typeof(d)==='undefined') return;
+	var data = d.split(':');
+	var profile = data[1];
+	var latency = data[2];
+	if(!profile || !latency) return;
+	db.get('testticketList:'+profile+':timestamp',function(err,reply){
+		if(!isFresh(reply,30000)) {
+			console.log("get: %s, %s",profile,latency);
+			fetchSummary(profile,latency);
+		}else{
+			console.log("skip: %s",profile);
+			popNext();
+		}		
+	});
+}
+
+function popNext() {
+	db.blpop('wantNewSummaryTest', 0, function(err, data) {
+		//console.log('processing: ' + data[1]);
+		newSummary(data[1]);
+	});
+}
+
+popNext();
