b3f7e45c5c0db357c4b55c434b351715fd885378
aver4919 <avery.scott@rackspace.com>
2014-04-27 09:32:49 -0500

    Genericize score function for use in any js

--- score/score.js
+++ score/score.js
@@ -1,45 +1,50 @@
-app.filter('score',function(){
-        return function(t) {
-            if (/^SCHLD \d+.*/.test(t.subject))
-                return '-';
-            var feedBack = 'Feedback Received';
-            var points = 0;
-            var base_score = 0;
+function baseScore(t) {
+    var base_score = 1;
 
-                        // customer feedback
-            if(/.*ALERT:Rackwatch.*All.*Services.*/i.test(t.subject))
-                base_score = 1200;
-            else if(/ALERT:.*SiteScope.*/i.test(t.subject))
-                base_score = 800;
-            else if(/.*customer_initiated.*/i.test(t.category))
-                base_score = 600;
-            else if(t.status == feedBack)
-                base_score = 600;
+    if(t.subject == undefined)
+       return undefined;
+    var feedBack = 'Feedback Received';
 
-            //create severity for cloud tickets
-            if (t.iscloud)
-                if (/Monitoring Alert .*/.test(t.subject))
-                    t.sev = 'emergency';
+    var sub = t['subject'];
+    var cat = t['category'];
 
-            //bolster score with severity
-            if(t.sev =='emergency')
-                points = 35;
-            else if(t.sev =='urgent')
-                points = 9;
-            else
-                points = 2.25;
+    // customer feedback
+    if(/.*ALERT:Rackwatch.*All.*Services.*/i.test(sub))
+        base_score = 1200;
+    else if(/ALERT:.*SiteScope.*/i.test(sub))
+        base_score = 800;
+    else if (/Monitoring Alert .*/.test(sub))
+        base_score = 700
+    else if(/.*customer_initiated.*/i.test(cat))
+        base_score = 600;
+    else if(t.status == feedBack)
+        base_score = 600;
+    else
+    return base_score;
+    return base_score/100;
+}
 
-            // score primarily based on minutes ... at least 2.
-            var minticks = ( t.age_seconds / 60) * 2;
-            minticks = Math.max(2,minticks);
+function ticketScore(t) {
+    if (/^SCHLD \d+.*/.test(t.subject))
+        return '-';
 
-            //data collected, time to calc
-            var score = base_score + (minticks * points);
-            //if($profile)
-            //    $score *= getScoreModifier($t,$profile);
-            score = parseInt(score);
+    if(t.base_score == undefined) 
+        t.base_score = baseScore(t);
 
-            return score;
-        }
-    }
-);
+    var points = 2.25;
+    if(t.severity =='emergency')
+        points = 35;
+    else if(t.severity =='urgent')
+        points = 9;
+
+    // score is primarily based on ticket age ...
+    var age = (Date.now() - t.intime) / 1000 / 60;
+
+    //data collected, time to calc
+    var score = t.base_score + (2 * age * points);
+    //var score = (t.base_score + ( points  * Math.log(Date.now() - t.intime) ) );
+    //if($profile)
+    //    $score *= getScoreModifier($t,$profile);
+
+    return score |0 ;
+}
